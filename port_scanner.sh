8fcaebd95ad5784c657cb3832e9ca0043a2cc3aa
paulino-s <paulinovasquez900@gmail.com>
2021-10-09 16:34:03 -0600

    shh start

--- port_scanner.sh
+++ port_scanner.sh
@@ -2,8 +2,8 @@
 if [ "$1" = "" ];
 then
 	echo "Uso $0 [IP]"
-	echo "Ejemplo $0 192.168.109.1"
+	echo "Ejemplo $0 192.168.1.1"
 else
 	echo "Por favor espere mientras se ejecuta el scanep..."
-	nc -nvz $1 1-100 > resultados.txt
+	nc -nvz $1 1-65535 > resultados.txt
 fi
